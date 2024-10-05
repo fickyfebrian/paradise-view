668a5d923dd10e25447fa9c7d681b052ba8d8c59
ficky febrian <iky.febrian@gmail.com>
2024-10-05 11:02:46 +0700

    Update Page Rooms

--- src/components/Rooms/ListRoom.jsx
+++ src/components/Rooms/ListRoom.jsx
@@ -5,35 +5,35 @@ import { IoTvOutline } from "react-icons/io5";
 import { PiShowerThin } from "react-icons/pi";
 
 const RoomCard = ({ image, title, price }) => (
-  <div className="bg-white overflow-hidden w-[350px] shadow-sm">
-    <img src={image} alt={title} className="w-full h-72 object-cover" />
+  <div className="bg-white overflow-hidden w-[490px] shadow-sm">
+    <img src={image} alt={title} className="w-full h-96 object-cover" />
     <div className="p-4">
       <div className="grid grid-cols-2">
         <div>
-          <h3 className="text-xl text-primary font-semibold">{title}</h3>
+          <h3 className="text-4xl text-primary font-semibold">{title}</h3>
         </div>
         <div className="text-right">
-          <p className="text-primary text-sm">Available: Yes</p>
+          <p className="text-primary text-l">Available: Yes</p>
         </div>
       </div>
 
-      <p className="text-2xl text-primary mt-2">₦{price.toLocaleString()}</p>
+      <p className="text-3xl text-primary mt-2">₦{price.toLocaleString()}</p>
       
       <div className="mt-4 border-t border-primary pt-4 w-full"></div>
       
       <div className="flex justify-between items-center mt-4">
         <div className="flex space-x-8">
-          <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center">
-            <IoTvOutline className="text-2xl text-primary" />
+          <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center">
+            <IoTvOutline className="text-4xl text-primary" />
           </div>
-          <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center">
-            <PiShowerThin className="text-2xl text-primary" />
+          <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center">
+            <PiShowerThin className="text-4xl text-primary" />
           </div>
-          <div className="w-10 h-10 bg-gray-100 rounded-full flex items-center justify-center">
-            <CiWifiOn className="text-2xl text-primary" />
+          <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center">
+            <CiWifiOn className="text-4xl text-primary" />
           </div>
         </div>
-        <button className="bg-primary text-white px-6 py-2 hover:bg-secondary transition">
+        <button className="bg-primary text-white text-xl px-6 py-2 hover:bg-secondary transition h-16 w-56">
           Book now
         </button>
       </div>
@@ -51,7 +51,7 @@ const ListRoom = () => {
   return (
     <div className="container mx-auto px-2 py-24">
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 justify-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24 justify-items-center">
           {rooms.map((room, index) => (
             <RoomCard key={index} {...room} />
           ))}
